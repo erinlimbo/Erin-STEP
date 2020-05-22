340fe5c00debc3ee0ae7fc403a54c7dee4a27362
Erin <erinrlimbo@gmail.com>
2020-05-21 17:10:46 -0700

    add image, and set this image to show/hide

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,12 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+function showPicture() {
+    var image = document.getElementById("portrait");
+    if (image.style.visibility === "visible") {
+        image.style.visibility = "hidden";
+    } else {
+        image.style.visibility = "visible"
+    }
+}
