27dbfddf1b83a48c71863868419fe566c6269d1e
Erin <erinrlimbo@gmail.com>
2020-05-28 11:00:11 -0700

    Create a Comments class.

---
+++ portfolio/src/main/java/com/google/sps/data/Comments.java
@@ -0,0 +1,38 @@
+package com.google.sps.data;
+
+import java.util.Date;
+
+/** Class containing the data for comments. */
+public final class Comments {
+
+    private final String author;
+    private final String comment;
+    private Date timeStamp;
+
+    /**
+     * @param {String} author Identifies the author of the comment.
+     * @param {!String} comment The comment.
+     */
+    public Comments(String author, String comment) {
+        this.author = author;
+        this.comment = comment;
+        this.timeStamp = new Date();
+    }
+
+
+    /** Getter method for author. */
+    public long getAuthor() {
+        return author;
+    }
+
+    /** Getter method for comment. */
+    public long getComment() {
+        return comment;
+    }
+
+    /** Getter method for timeStamp. */
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+}
