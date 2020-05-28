41f10a07c48fd46a740efc5409bff8489cd3de28
Erin <erinrlimbo@gmail.com>
2020-05-28 16:13:24 -0700

    Indented to 2 spaces.

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -5,34 +5,32 @@ import java.util.Date;
 /** Class containing the data for comments. */
 public final class Comment {
 
-    private final String author;
-    private final String comment;
-    private Date timeStamp;
+  private final String author;
+  private final String comment;
+  private Date timeStamp;
 
-    /**
-     * @param {String} author Identifies the author of the comment.
-     * @param {!String} comment The comment.
-     */
-    public Comment(String author, String comment) {
-        this.author = author;
-        this.comment = comment;
-        this.timeStamp = new Date();
-    }
+  /**
+   * @param {String} author Identifies the author of the comment.
+   * @param {!String} comment The comment.
+   */
+  public Comment(String author, String comment) {
+    this.author = author;
+    this.comment = comment;
+    this.timeStamp = new Date();
+}
 
+  /** Getter method for author. */
+  public String getAuthor() {
+    return author;
+  }
 
-    /** Getter method for author. */
-    public String getAuthor() {
-        return author;
-    }
+  /** Getter method for comment. */
+  public String getComment() {
+    return comment;
+  }
 
-    /** Getter method for comment. */
-    public String getComment() {
-        return comment;
-    }
-
-    /** Getter method for timeStamp. */
-    public Date getTimeStamp() {
-        return timeStamp;
-    }
-
+  /** Getter method for timeStamp. */
+  public Date getTimeStamp() {
+    return timeStamp;
+  }
 }
